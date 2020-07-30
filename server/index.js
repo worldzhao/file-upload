b183aef51982052745387a0a66e44041191a5f14
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2020-07-30 14:01:28 +0800

    feat: 分片上传

---
+++ server/index.js
@@ -0,0 +1,102 @@
+const path = require("path");
+const fs = require("fs-extra");
+const Koa = require("koa");
+const cors = require("@koa/cors");
+const Router = require("@koa/router");
+const koaBody = require("koa-body");
+const koaStatic = require("koa-static");
+
+const app = new Koa();
+const router = new Router();
+
+const uploadDir = path.join(__dirname, "public/uploads");
+
+app.use(koaStatic(path.resolve(__dirname, "./public")));
+
+app.use(
+  koaBody({
+    // 支持文件格式
+    multipart: true,
+    formidable: {
+      // 上传目录
+      uploadDir,
+      // 保留文件扩展名
+      keepExtensions: true,
+    },
+  })
+);
+
+// 上传切片接口
+router.post("/upload-chunk", (ctx, next) => {
+  // 如果切片目录不存在 创建切片目录
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  const { hash, filename } = ctx.request.body;
+  const { chunk } = ctx.request.files;
+  const chunkDir = path.resolve(uploadDir, `${filename}_temp`);
+
+  // 如果切片目录不存在 创建切片目录
+  if (!fs.existsSync(chunkDir)) {
+    fs.mkdirSync(chunkDir);
+  }
+
+  fs.moveSync(chunk.path, `${chunkDir}/${hash}`);
+
+  // 将切片移动至切片目录
+  ctx.body = {
+    code: 0,
+    msg: "上传成功",
+    data: null,
+  };
+});
+
+// 合并接口
+router.get("/merge", async (ctx, next) => {
+  const { filename, size } = ctx.request.query;
+  const filePath = path.resolve(uploadDir, filename);
+  await mergeChunks(filePath, filename, size);
+  ctx.body = {
+    code: 0,
+    msg: "合并成功",
+    data: `${ctx.origin}/uploads/${filename}`,
+  };
+});
+
+app.use(cors()).use(router.routes()).use(router.allowedMethods());
+
+app.listen(3001);
+
+console.log("node server in listening at port 3001");
+
+// 合并切片
+async function mergeChunks(filePath, filename, size) {
+  const chunkDir = path.resolve(uploadDir, `${filename}_temp`);
+  const chunkPaths = fs.readdirSync(chunkDir);
+
+  // 根据切片下标进行排序
+  chunkPaths.sort((a, b) => a.slice(-1)[0] - b.slice(-1)[0]);
+
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) =>
+      pipeStream(
+        path.resolve(chunkDir, chunkPath),
+        fs.createWriteStream(filePath, {
+          start: index * size,
+          end: (index + 1) * size,
+        })
+      )
+    )
+  );
+  fs.rmdirSync(chunkDir);
+}
+
+const pipeStream = (path, writeStream) =>
+  new Promise((resolve) => {
+    const readStream = fs.createReadStream(path);
+    readStream.on("end", () => {
+      fs.unlinkSync(path);
+      resolve();
+    });
+    readStream.pipe(writeStream);
+  });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "antd/es/button/style/css";
+import "./App.css";
 
 function App() {
+  // const [file, setFile] = useState<File | null>(null);
+
+  // 监听input事件获取文件
+  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) return;
+    const file = e.target.files[0];
+    // setFile(file);
+    uploadFile(file);
+  };
+
+  // 上传文件
+  const uploadFile = async (file: File) => {
+    const chunks = createChunks(file);
+    const chunksWithHash = chunks.map(({ file: chunk }, index) => ({
+      chunk: chunk,
+      hash: `${file.name}-${index}`,
+    }));
+    await uploadChunks(chunksWithHash, file);
+    await notify2Merge(file);
+  };
+
+  // 上传切片
+  const uploadChunks = (
+    chunksWithHash: { chunk: Blob; hash: string }[],
+    file: File
+  ) => {
+    const parallelPromises = chunksWithHash
+      .map(({ chunk, hash }) => {
+        const fd = new FormData();
+        fd.append("chunk", chunk);
+        fd.append("hash", hash);
+        fd.append("filename", file.name);
+        return fd;
+      })
+      .map((fd) =>
+        axios.request({
+          method: "post",
+          url: "http://localhost:3001/upload-chunk",
+          data: fd,
+        })
+      );
+
+    return Promise.all(parallelPromises);
+  };
+
+  // 通知后端合并切片
+  const notify2Merge = (file: File) => {
+    return axios.request({
+      method: "get",
+      url: "http://localhost:3001/merge",
+      params: { filename: file.name, size: SIZE },
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={onFileInputChange} />
     </div>
   );
 }
 
 export default App;
+
+const SIZE = 10 * 1024 * 1024; // 切片大小
+
+// 生成分片
+function createChunks(file: File, size = SIZE) {
+  const chunks = [];
+  let cur = 0;
+  while (cur < file.size) {
+    chunks.push({ file: file.slice(cur, cur + size) });
+    cur += size;
+  }
+  return chunks;
+}
